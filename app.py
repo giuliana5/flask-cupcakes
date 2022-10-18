9b203f7f684b8e4bd3facb197944482568c09721
gabe <playwigs@gmail.com>
2022-10-17 21:34:54 -0600

    Created route to delete cupcake & return confirmation message in json.

--- app.py
+++ app.py
@@ -59,4 +59,15 @@ def update_cupcake(id):
     db.session.commit()
 
     return jsonify(cupcake=cupcake.serialize())
+
+@app.route("/api/cupcakes/<int:id>", methods=["DELETE"])
+def delete_cupcake(id):
+    """Deletes the selected cupcake."""
+
+    cupcake = Cupcake.query.get_or_404(id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="deleted")
     
